41d463dae0ddb772a8fd4208ceec1a245370c231
Jordan Welch <jwelch@highlandsolutions.com>
2021-02-23 21:50:05 -0500

    4 > 2 spaces

--- resources/views/livewire/chores/save.blade.php
+++ resources/views/livewire/chores/save.blade.php
@@ -1,27 +1,27 @@
 <div class="p-20">
-    <div class="p-12 bg-white rounded-lg shadow-md">
-        <form wire:submit.prevent="save">
-            <h1 class="pb-4 text-2xl">New Chore</h1>
-            <div>
-                <label for="title">Title</label>
-                <input wire:model="chore.title" id="title" type="text">
-            </div>
-            <div>
-                <label for="description">Description</label>
-                <input wire:model="chore.description" id="description" type="text">
-            </div>
-            <select wire:model="chore.frequency">
-                @foreach (\App\Models\Chore::FREQUENCIES as $key => $frequencyOption)
-                    <option wire:key="$key" value={{ $key }}>{{ $frequencyOption }}</option>
-                @endforeach
-            </select>
-            <input type="submit"/>
-        </form>
+  <div class="p-12 bg-white rounded-lg shadow-md">
+    <form wire:submit.prevent="save">
+      <h1 class="pb-4 text-2xl">New Chore</h1>
+      <div>
+        <label for="title">Title</label>
+        <input wire:model="chore.title" id="title" type="text">
+      </div>
+      <div>
+        <label for="description">Description</label>
+        <input wire:model="chore.description" id="description" type="text">
+      </div>
+      <select wire:model="chore.frequency">
+        @foreach (\App\Models\Chore::FREQUENCIES as $key => $frequencyOption)
+          <option wire:key="$key" value={{ $key }}>{{ $frequencyOption }}</option>
+        @endforeach
+      </select>
+      <input type="submit"/>
+    </form>
 
-        @if(isset($errors))
-            @foreach ($errors->all() as $error)
-                <div>{{ $error }}</div>
-            @endforeach
-        @endif
-    </div>
+    @if(isset($errors))
+      @foreach ($errors->all() as $error)
+        <div>{{ $error }}</div>
+      @endforeach
+    @endif
+  </div>
 </div>
