d4fe2812f95b36e00bf600f9d703de1a05c43e08
Jordan Welch <jwelch@highlandsolutions.com>
2021-02-25 20:02:40 -0500

    Clarify Due Date

--- resources/views/livewire/chores/index.blade.php
+++ resources/views/livewire/chores/index.blade.php
@@ -26,7 +26,7 @@
                 </th>
 
                 <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
-                  Due Date
+                  Next Due Date
                 </th>
 
                 <th scope="col" class="relative px-6 py-3">
