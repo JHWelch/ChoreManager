a907ad03ed4848cef97d61257cf6edb8f1bf5438
Jordan Welch <jwelch@highlandsolutions.com>
2021-03-07 21:26:57 -0600

    Fix filter bug

--- resources/views/livewire/chore-instances/index.blade.php
+++ resources/views/livewire/chore-instances/index.blade.php
@@ -20,23 +20,23 @@
   @else
     <nav class="relative h-full overflow-y-auto" aria-label="Chores">
       @foreach ($chore_instance_groups as $date => $chore_instances)
-        @php
-          $now = \Carbon\Carbon::now();
-          $due_date = \Carbon\Carbon::parse($date);
-          $difference = $due_date->diff($now)->days < 1
-            ? 'today'
-            : $due_date->diffForHumans();
-        @endphp
-
-        <div class="sticky top-0 z-10 px-6 py-1 text-sm font-medium text-gray-500 border-t border-b border-gray-200 bg-gray-50">
-          <h3>{{ $difference }}</h3>
+        <div wire:key="{{ $date }}">
+          @php
+            $now = \Carbon\Carbon::now();
+            $due_date = \Carbon\Carbon::parse($date);
+            $difference = $due_date->diff($now)->days < 1
+              ? 'today'
+              : $due_date->diffForHumans();
+          @endphp
+          <div class="sticky top-0 z-10 px-6 py-1 text-sm font-medium text-gray-500 border-t border-b border-gray-200 bg-gray-50">
+            <h3>{{ $difference }}</h3>
+          </div>
+          <ul class="relative z-0 divide-y divide-gray-200">
+            @foreach ($chore_instances as $chore_instance)
+              <livewire:chore-instances.index-line :key="$chore_instance['chore_instance_id']" :chore="$chore_instance" />
+            @endforeach
+          </ul>
         </div>
-
-        <ul class="relative z-0 divide-y divide-gray-200">
-          @foreach ($chore_instances as $chore_instance)
-            <livewire:chore-instances.index-line :key="$chore_instance['chore_instance_id']" :chore="$chore_instance" />
-          @endforeach
-        </ul>
       @endforeach
     </nav>
   @endif
