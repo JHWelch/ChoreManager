a6f34b1026d6374ce49674b129e984c2bd1cde70
Jordan Welch <jwelch@highlandsolutions.com>
2021-03-19 21:59:41 -0500

    Split input into bare

---
+++ resources/views/components/form/bare/input.blade.php
@@ -0,0 +1,19 @@
+@props([
+  'label'       => ucfirst($name),
+  'prefix'      => '',
+  'placeholder' => '',
+  'type'        => 'text',
+  'name',
+])
+
+<div class="mt-1">
+  <input
+    {{ $attributes->merge([
+      'wire:model.defer' => $prefix . '.' . $name,
+      'type'             => $type,
+      'class'            => 'shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md',
+      'id'               => $name,
+      'placeholder'      => $placeholder,
+    ]) }}
+  />
+</div>
