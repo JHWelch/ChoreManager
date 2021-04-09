a7207fa56d9f7f9dead3665d2ba3cd5585e302ba
Jordan Welch <jwelch@highlandsolutions.com>
2021-04-08 22:03:24 -0500

    Separate Next-Instance to component

--- resources/views/livewire/chores/show.blade.php
+++ resources/views/livewire/chores/show.blade.php
@@ -34,26 +34,9 @@
               </div>
             </div>
             <aside class="mt-8 xl:hidden">
-              @if ($chore_instance)
-                <div>
-                  <h2 class="text-sm font-medium text-gray-500">Next Instance</h2>
+              <x-chore-instances.next-instance :instance="$chore_instance" class="mb-6" />
 
-                  <div class="mt-3 space-y-5">
-                    <div class="flex items-center space-x-2">
-                      <x-icons.calendar solid="true" class="w-5 h-5 text-gray-400" />
-                      <span class="text-sm font-medium text-gray-900">
-                        Due on
-                        <time datetime="{{ $chore_instance->due_date->toDateString() }}">
-                          {{ $chore_instance->due_date->toFormattedDateString() }}
-                        </time>
-                      </span>
-                    </div>
-                    <x-users.avatar-line :user="$chore_instance->user" />
-                  </div>
-                </div>
-              @endif
-
-              <div class="py-6 mt-6 space-y-8 border-t border-b border-gray-200">
+              <div class="{{ $chore_instance ? 'border-t py-6' : '' }} space-y-8 border-b border-gray-200">
                 <div>
                   <h2 class="text-sm font-medium text-gray-500">Owner</h2>
 
@@ -65,6 +48,7 @@
             </aside>
             <div class="py-3 xl:pt-6 xl:pb-0">
               <h2 class="sr-only">Description</h2>
+
               <div class="prose max-w-none">
                 {{ $chore->description }}
               </div>
@@ -77,6 +61,7 @@
               <div class="pb-4">
                 <h2 id="activity-title" class="text-lg font-medium text-gray-900">Activity</h2>
               </div>
+
               <div class="pt-6">
                 <!-- Activity feed-->
                 <div class="flow-root">
@@ -84,6 +69,7 @@
                     <li>
                       <div class="relative pb-8">
                         <span class="absolute top-5 left-5 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
+
                         <div class="relative flex items-start space-x-3">
                           <div class="relative">
                             <img class="flex items-center justify-center w-10 h-10 bg-gray-400 rounded-full ring-8 ring-white" src="https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80" alt="">
@@ -100,6 +86,7 @@
                               <div class="text-sm">
                                 <a href="#" class="font-medium text-gray-900">Eduardo Benz</a>
                               </div>
+
                               <p class="mt-0.5 text-sm text-gray-500">
                                 Commented 6d ago
                               </p>
@@ -256,88 +243,18 @@
         </section>
       </div>
       <aside class="hidden xl:block xl:pl-8">
-        <h2 class="sr-only">Details</h2>
-        <div class="space-y-5">
-          <div class="flex items-center space-x-2">
-            <!-- Heroicon name: solid/lock-open -->
-            <svg class="w-5 h-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path d="M10 2a5 5 0 00-5 5v2a2 2 0 00-2 2v5a2 2 0 002 2h10a2 2 0 002-2v-5a2 2 0 00-2-2H7V7a3 3 0 015.905-.75 1 1 0 001.937-.5A5.002 5.002 0 0010 2z" />
-            </svg>
-            <span class="text-sm font-medium text-green-700">Open Issue</span>
-          </div>
-          <div class="flex items-center space-x-2">
-            <!-- Heroicon name: solid/chat-alt -->
-            <svg class="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clip-rule="evenodd" />
-            </svg>
-            <span class="text-sm font-medium text-gray-900">4 comments</span>
-          </div>
-          <div class="flex items-center space-x-2">
-            <!-- Heroicon name: solid/calendar -->
-            <svg class="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
-            </svg>
-            <span class="text-sm font-medium text-gray-900">
-              Created on
-              <time datetime="{{ $chore->created_at->toDateString() }}">
-                {{ $chore->created_at->toFormattedDateString() }}
-              </time>
-            </span>
-          </div>
-        </div>
-        <div class="py-6 mt-6 space-y-8 border-t border-gray-200">
+        <x-chore-instances.next-instance :instance="$chore_instance" class="mb-6" />
+
+        <div class="{{ $chore_instance ? 'border-t py-6' : '' }} space-y-8 border-gray-200">
           <div>
             <h2 class="text-sm font-medium text-gray-500">Owner</h2>
-            <ul class="mt-3 space-y-3">
-              <li class="flex justify-start">
-                <a href="#" class="flex items-center space-x-3">
-                  <div class="flex-shrink-0">
-                    <img class="w-5 h-5 rounded-full" src="https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80" alt="">
-                  </div>
-                  <div class="text-sm font-medium text-gray-900">Eduardo Benz</div>
-                </a>
-              </li>
-            </ul>
+
+            <div class="mt-3">
+              <x-users.avatar-line :user="$chore->user" />
+            </div>
           </div>
         </div>
       </aside>
     </div>
   </div>
 </main>
-
-{{-- <div
-  x-data="{ x: false }"
-  x-on:checked.window="$wire.complete()"
->
-  <div class="p-8 mt-4 bg-white shadow-md rounded-xl">
-    <div class="space-y-2">
-      <div class="flex justify-between">
-        <h1 class="text-2xl">
-          {{ $chore->title }}
-        </h1>
-
-        <div class="flex flex-col">
-          <x-chore-checkbox :checked="$chore_instance->is_completed" />
-
-          <div>
-            {{ $chore_instance->due_date->format('n/j') }}
-          </div>
-        </div>
-      </div>
-
-      <div class="text-gray-600">
-        {{ $chore->frequency }}
-      </div>
-
-      <a href="{{ route('chores.edit', $chore) }}" class="inline-flex justify-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900">
-        <x-icons.pencil class="w-5 h-5 mr-2 -ml-1 text-gray-400"/>
-
-        <span>Edit</span>
-      </a>
-
-      <div class="prose">
-        {{ $chore->description }}
-      </div>
-    </div>
-  </div>
-</div> --}}
