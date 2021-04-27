c6d35f30b416985740b5d99c0834fc88259cc157
Jordan Welch <jwelch@highlandsolutions.com>
2021-04-26 22:13:00 -0500

    Placeholder favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="icon" href="https://fav.farm/🧹" />
+
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
