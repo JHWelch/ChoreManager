64059f07d88a2add446a18dd3a9f39a7f9da4efb
Jordan Welch <jwelch@highlandsolutions.com>
2021-04-28 21:28:09 -0500

    Refactor exisiting blade components from 4 to 2 spaces

--- resources/views/vendor/jetstream/components/dialog-modal.blade.php
+++ resources/views/vendor/jetstream/components/dialog-modal.blade.php
@@ -1,17 +1,17 @@
 @props(['id' => null, 'maxWidth' => null])
 
 <x-jet-modal :id="$id" :maxWidth="$maxWidth" {{ $attributes }}>
-    <div class="px-6 py-4">
-        <div class="text-lg">
-            {{ $title }}
-        </div>
-
-        <div class="mt-4">
-            {{ $content }}
-        </div>
+  <div class="px-6 py-4">
+    <div class="text-lg">
+      {{ $title }}
     </div>
 
-    <div class="px-6 py-4 bg-gray-100 text-right">
-        {{ $footer }}
+    <div class="mt-4">
+      {{ $content }}
     </div>
+  </div>
+
+  <div class="px-6 py-4 text-right bg-gray-100">
+    {{ $footer }}
+  </div>
 </x-jet-modal>
