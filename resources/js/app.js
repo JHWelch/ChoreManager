4d9ff89594897477e95510e85cf631f221e1f4ad
Jordan Welch <jwelch@highlandsolutions.com>
2021-08-02 21:28:54 -0500

    upgrade alpine to 3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,8 @@
-require('./bootstrap');
+import Alpine from 'alpinejs';
 
-require('alpinejs');
+
+
+
+window.Alpine = Alpine;
+Alpine.start();
+require('./bootstrap');
--- resources/views/vendor/jetstream/components/action-message.blade.php
+++ resources/views/vendor/jetstream/components/action-message.blade.php
@@ -2,7 +2,7 @@
 
 <div x-data="{ shown: false, timeout: null }"
   x-init="@this.on('{{ $on }}', () => { clearTimeout(timeout); shown = true; timeout = setTimeout(() => { shown = false }, 2000);  })"
-  x-show.transition.opacity.out.duration.1500ms="shown"
+  x-transition.opacity.out.duration.1500ms="shown"
   style="display: none;"
   {{ $attributes->merge(['class' => 'text-sm text-gray-600']) }}>
   {{ $slot->isEmpty() ? 'Saved.' : $slot }}
