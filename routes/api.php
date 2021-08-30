ebfbed97b8a72576f97591f124c1b7149e5113ad
Jordan Welch <jwelch@highlandsolutions.com>
2021-08-30 09:59:06 -0500

    Refactor token route to Controller

--- routes/api.php
+++ routes/api.php
@@ -26,25 +26,10 @@ Route::get('/ical/{token}', [\App\Http\Controllers\Api\ICalendarController::clas
 /*
 * MOBILE API
 */
+Route::post('/token', [App\Http\Controllers\Api\AuthController::class, 'store'])
+    ->name('api.token');
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/chores', [\App\Http\Controllers\Api\ChoreController::class, 'index'])
         ->name('api.chores.index');
 });
-
-Route::post('/token', function (Request $request) {
-    $request->validate([
-        'email'       => 'required|email',
-        'password'    => 'required',
-        'device_name' => 'required',
-    ]);
-
-    $user = User::where('email', $request->email)->first();
-
-    if (! $user || ! Hash::check($request->password, $user->password)) {
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
-    }
-
-    return $user->createToken($request->device_name)->plainTextToken;
-})->name('api.token');
