2fd2d922ee09900743530b3cd2714ef9f9807d34
Jordan Welch <jordanhwelch@gmail.com>
2022-01-09 00:01:13 -0600

    Looking a little different

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Livewire\CalendarTokens\Index as CalendarTokensIndex;
+use App\Http\Livewire\ChoreInstances\Index as ChoreInstancesIndex;
+use App\Http\Livewire\Chores\Index as ChoresIndex;
+use App\Http\Livewire\Chores\Save as ChoresSave;
+use App\Http\Livewire\Chores\Show as ChoresShow;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,25 +20,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/', fn () => redirect(route('dashboard')));
+    Route::get('/dashboard', ChoreInstancesIndex::class)->name('dashboard');
 
-    Route::get('/dashboard', \App\Http\Livewire\ChoreInstances\Index::class)
-        ->name('dashboard');
+    Route::get('/chore_instances', ChoreInstancesIndex::class)->name('chore_instances.index');
 
-    Route::get('/chores', \App\Http\Livewire\Chores\Index::class)
-        ->name('chores.index');
+    Route::get('/chores', ChoresIndex::class)->name('chores.index');
+    Route::get('/chores/create', ChoresSave::class)->name('chores.create');
+    Route::get('/chores/{chore}', ChoresShow::class)->name('chores.show');
+    Route::get('/chores/{chore}/edit', ChoresSave::class)->name('chores.edit');
 
-    Route::get('/chores/create', \App\Http\Livewire\Chores\Save::class)
-        ->name('chores.create');
-
-    Route::get('/chores/{chore}/edit', \App\Http\Livewire\Chores\Save::class)
-        ->name('chores.edit');
-
-    Route::get('/chores/{chore}', \App\Http\Livewire\Chores\Show::class)
-        ->name('chores.show');
-
-    Route::get('/chore_instances', \App\Http\Livewire\ChoreInstances\Index::class)
-        ->name('chore_instances.index');
-
-    Route::get('/calendar_links', \App\Http\Livewire\CalendarTokens\Index::class)
-        ->name('calendar_tokens.index');
+    Route::get('/calendar_links', CalendarTokensIndex::class)->name('calendar_tokens.index');
 });
