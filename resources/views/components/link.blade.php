a2c086b0c3130f07a805e849f483787c0c914b54
Jordan Welch <jordanhwelch@gmail.com>
2022-12-01 22:33:46 -0600

    Update button color

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,3 +1,7 @@
-<a {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-purple-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-700 active:bg-purple-900 focus:outline-none focus:border-purple-900 focus:shadow-outline-purple disabled:opacity-25 transition ease-in-out duration-150']) }}>
+<a {{
+  $attributes->merge([
+    'class' => 'inline-flex items-center rounded-md border border-transparent bg-purple-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2'
+  ])
+}}>
     {{ $slot }}
 </a>
