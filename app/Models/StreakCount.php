5c01f33e23b36d2e3df0eb327d3b5da1dab60f90
Jordan Welch <jordanhwelch@gmail.com>
2022-12-20 20:42:40 -0600

    add ide helper comments

--- app/Models/StreakCount.php
+++ app/Models/StreakCount.php
@@ -16,6 +16,20 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|StreakCount query()
  * @mixin \Eloquent
  * @property-read \App\Models\Team $team
+ * @property int $id
+ * @property string|null $ended_at
+ * @property int $count
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int|null $user_id
+ * @property int|null $team_id
+ * @method static \Illuminate\Database\Eloquent\Builder|StreakCount whereCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|StreakCount whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|StreakCount whereEndedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|StreakCount whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|StreakCount whereTeamId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|StreakCount whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|StreakCount whereUserId($value)
  */
 class StreakCount extends Model
 {
