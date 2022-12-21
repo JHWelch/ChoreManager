8f12605bd66616a43b30b1a6e7ebf6c445961a91
Jordan Welch <jordanhwelch@gmail.com>
2022-12-20 20:09:15 -0600

    comment unused methods

--- app/Http/Controllers/Api/ChoreInstanceController.php
+++ app/Http/Controllers/Api/ChoreInstanceController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\ChoreInstance;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ChoreInstanceController extends Controller
@@ -23,69 +21,69 @@ class ChoreInstanceController extends Controller
             ->get();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+    // /**
+    //  * Show the form for creating a new resource.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function create()
+    // {
+    //     //
+    // }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    // /**
+    //  * Store a newly created resource in storage.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function store(Request $request)
+    // {
+    //     //
+    // }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\ChoreInstance  $choreInstance
-     * @return \Illuminate\Http\Response
-     */
-    public function show(ChoreInstance $choreInstance)
-    {
-        //
-    }
+    // /**
+    //  * Display the specified resource.
+    //  *
+    //  * @param  \App\Models\ChoreInstance  $choreInstance
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function show(ChoreInstance $choreInstance)
+    // {
+    //     //
+    // }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\ChoreInstance  $choreInstance
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(ChoreInstance $choreInstance)
-    {
-        //
-    }
+    // /**
+    //  * Show the form for editing the specified resource.
+    //  *
+    //  * @param  \App\Models\ChoreInstance  $choreInstance
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function edit(ChoreInstance $choreInstance)
+    // {
+    //     //
+    // }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\ChoreInstance  $choreInstance
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, ChoreInstance $choreInstance)
-    {
-        //
-    }
+    // /**
+    //  * Update the specified resource in storage.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @param  \App\Models\ChoreInstance  $choreInstance
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function update(Request $request, ChoreInstance $choreInstance)
+    // {
+    //     //
+    // }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\ChoreInstance  $choreInstance
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(ChoreInstance $choreInstance)
-    {
-        //
-    }
+    // /**
+    //  * Remove the specified resource from storage.
+    //  *
+    //  * @param  \App\Models\ChoreInstance  $choreInstance
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function destroy(ChoreInstance $choreInstance)
+    // {
+    //     //
+    // }
 }
