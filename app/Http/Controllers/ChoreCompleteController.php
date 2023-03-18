d0d8dae8d1a46291325176c9d4f2199037187a82
Jordan Welch <jordanhwelch@gmail.com>
2023-03-18 15:00:08 -0500

    Add type

--- app/Http/Controllers/ChoreCompleteController.php
+++ app/Http/Controllers/ChoreCompleteController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chore;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class ChoreCompleteController extends Controller
 {
-    public function index(Request $request, Chore $chore)
+    public function index(Request $request, Chore $chore): RedirectResponse
     {
         session()->flash('complete', true);
 
