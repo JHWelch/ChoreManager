c99887186a62c7842ce18209ac67c45d722447b6
Jordan Welch <jordanhwelch@gmail.com>
2023-03-18 13:52:46 -0500

    prettier imports

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Livewire\CalendarTokens\Index as CalendarTokensIndex;
-use App\Http\Livewire\ChoreInstances\Index as ChoreInstancesIndex;
-use App\Http\Livewire\Chores\Index as ChoresIndex;
-use App\Http\Livewire\Chores\Save as ChoresSave;
-use App\Http\Livewire\Chores\Show as ChoresShow;
+use App\Http\Livewire\CalendarTokens;
+use App\Http\Livewire\ChoreInstances;
+use App\Http\Livewire\Chores;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,14 +20,14 @@ Route::get('/health-check', fn () => response()->json(['status' => 'ok']))->name
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/', fn () => redirect(route('dashboard')));
-    Route::get('/dashboard', ChoreInstancesIndex::class)->name('dashboard');
+    Route::get('/dashboard', ChoreInstances\Index::class)->name('dashboard');
 
-    Route::get('/chore_instances', ChoreInstancesIndex::class)->name('chore_instances.index');
+    Route::get('/chore_instances', ChoreInstances\Index::class)->name('chore_instances.index');
 
-    Route::get('/chores', ChoresIndex::class)->name('chores.index');
-    Route::get('/chores/create', ChoresSave::class)->name('chores.create');
-    Route::get('/chores/{chore}', ChoresShow::class)->name('chores.show');
-    Route::get('/chores/{chore}/edit', ChoresSave::class)->name('chores.edit');
+    Route::get('/chores', Chores\Index::class)->name('chores.index');
+    Route::get('/chores/create', Chores\Save::class)->name('chores.create');
+    Route::get('/chores/{chore}', Chores\Show::class)->name('chores.show');
+    Route::get('/chores/{chore}/edit', Chores\Save::class)->name('chores.edit');
 
-    Route::get('/calendar_links', CalendarTokensIndex::class)->name('calendar_tokens.index');
+    Route::get('/calendar_links', CalendarTokens\Index::class)->name('calendar_tokens.index');
 });
