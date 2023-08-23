f66cebc53349e199a48dbc523ede6e2febc5afa9
Jordan Welch <jordanhwelch@gmail.com>
2023-08-23 14:29:32 -0500

    Add admin team to created user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,22 +19,35 @@ class DatabaseSeeder extends Seeder
             return;
         }
 
-        User::factory()
+        $this->call(AdminTeamSeeder::class);
+
+        $user = User::factory()
             ->has(
                 Team::factory()
                     ->state(function (array $attributes, User $user) {
                         return ['user_id' => $user->id];
                     })
                     ->hasAttached(User::factory()->count(10), ['role' => 'editor']),
-                'ownedTeams'
+                relationship: 'ownedTeams'
             )
             ->create([
                 'name'     => 'John Smith',
                 'email'    => 'jsmith@example.com',
                 'password' => bcrypt('password'),
-            ])
-                ->switchTeam(Team::first());
+            ]);
+
+        $user->switchTeam(Team::firstWhere('user_id', $user->id));
+        $user->teams()->attach($this->adminTeam());
 
         $this->call(ChoreSeeder::class);
+    }
+
+    protected function adminTeam(): Team
+    {
+        if (is_null($team = Team::adminTeam())) {
+            throw new \Exception('Admin team not found');
+        }
+
+        return $team;
     }
 }
