d6f49bfb2cebf07710e744fdb88a2fed5f8d61af
Jordan Welch <jordan@shelterluv.com>
2024-01-17 10:05:11 -0600

    Wrap all first level ifs

--- resources/views/components/chore-instances/next-instance.blade.php
+++ resources/views/components/chore-instances/next-instance.blade.php
@@ -3,24 +3,26 @@
   'class' => ''
 ])
 
-@if ($instance)
-  <div class="{{ $class }}">
-    <h2 class="text-sm font-medium text-gray-500">Next Instance</h2>
+<div>
+  @if ($instance)
+    <div class="{{ $class }}">
+      <h2 class="text-sm font-medium text-gray-500">Next Instance</h2>
 
-    <div class="mt-3 space-y-5">
-      <div class="flex items-center space-x-2">
-        <x-icons.calendar solid="true" class="w-5 h-5 text-gray-400" />
+      <div class="mt-3 space-y-5">
+        <div class="flex items-center space-x-2">
+          <x-icons.calendar solid="true" class="w-5 h-5 text-gray-400" />
 
-        <span class="text-sm font-medium text-gray-900">
-          Due on
+          <span class="text-sm font-medium text-gray-900">
+            Due on
 
-          <time datetime="{{ $instance->due_date->toDateString() }}">
-            {{ $instance->due_date->toFormattedDateString() }}
-          </time>
-        </span>
+            <time datetime="{{ $instance->due_date->toDateString() }}">
+              {{ $instance->due_date->toFormattedDateString() }}
+            </time>
+          </span>
+        </div>
+
+        <x-users.avatar-line :user="$instance->user" />
       </div>
-
-      <x-users.avatar-line :user="$instance->user" />
     </div>
-  </div>
-@endif
+  @endif
+</div>
