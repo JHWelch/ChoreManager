6aa55b5c714dc51501064b5a16965c17083322f9
Jordan Welch <jordanhwelch@gmail.com>
2024-08-11 17:32:30 -0500

    update migrations for new style

--- database/migrations/2021_04_10_094719_update_chores_table_make_description_longer.php
+++ database/migrations/2021_04_10_094719_update_chores_table_make_description_longer.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('chores', function (Blueprint $table) {
-            $table->string('description', 5000)->change();
+            $table->string('description', 5000)->nullable()->change();
         });
     }
 };
