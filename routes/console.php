fbd5115abf247f1a5aad0d42ffb776dc204c2067
Shift <shift@laravelshift.com>
2024-08-11 21:50:30 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,26 @@
 <?php
 
+use App\Actions\Schedule\CountStreaks;
+use App\Actions\Schedule\SendDailyDigest;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+// If Demo is enabled reseed database daily.
+Schedule::command('migrate:fresh --force')
+    ->daily()
+    ->when(fn () => config('demo.enabled'))
+    ->then(function () {
+        $this->call('db:seed --force --class=DemoSeeder');
+    });
+
+Schedule::call(new SendDailyDigest)
+    ->dailyAt('07:00');
+
+Schedule::call(new CountStreaks)
+    ->dailyAt('02:00');
