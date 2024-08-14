a3de5880f234fc55a485bd39b6b8bcc236e79455
Jordan Welch <jordanhwelch@gmail.com>
2024-08-13 19:57:00 -0500

    Fix types

--- app/Models/Chore.php
+++ app/Models/Chore.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use App\Enums\Frequency;
 use App\Enums\FrequencyType;
 use Database\Factories\ChoreFactory;
-use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
