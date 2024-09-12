266dfa864a4ca6dbe2262e56ce89ea97ae87cdb1
Jordan Welch <jordanhwelch@gmail.com>
2024-09-11 21:16:38 -0500

    update to @class syntax

--- resources/views/components/button-group-button.blade.php
+++ resources/views/components/button-group-button.blade.php
@@ -4,25 +4,17 @@
   'position' => 'center'
 ])
 
-@php
-  $class = 'relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium bg-white border border-gray-300 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-purple-500 focus:border-purple-500';
-
-  if ($position === 'left') {
-    $class = $class . ' rounded-l-md';
-  } else if ($position === 'right') {
-    $class = $class . ' rounded-r-md';
-  } else {
-    $class = '';
-  }
-
-  $class = $class . ' ' . ($selected ? 'text-gray-800' : 'text-gray-500');
-@endphp
-
 <button
   wire:click="{{ $click }}"
   type="button"
-  class="{{ $class }}"
   aria-current="{{ $selected ? 'true' : 'false' }}"
+  @class([
+    'relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium bg-white border border-gray-300 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-purple-500 focus:border-purple-500',
+    'rounded-l-md' => $position === 'left',
+    'rounded-r-md' => $position === 'right',
+    'text-gray-800' => $selected,
+    'text-gray-500' => !$selected,
+  ])
 >
   {{ $slot }}
 
