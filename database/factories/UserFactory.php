bb23e44ec67eefb344b3f193607198522e9e5787
Jordan Welch <jordanhwelch@gmail.com>
2024-09-11 22:21:18 -0500

    ignore error

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -60,7 +60,7 @@ class UserFactory extends Factory
 
         return $this->has(
             Team::factory()
-                ->state(fn (array $attributes, User $user) => [
+                ->state(fn (array $attributes, User $user) => [ // @phpstan-ignore-line
                     'name' => $user->name . '\'s Team',
                     'user_id' => $user->id,
                     'personal_team' => true,
