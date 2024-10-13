c243dd8822899252fa5547dfc32b0639ea3c9e43
Jordan Welch <jordanhwelch@gmail.com>
2024-10-13 18:02:41 -0500

    Install vite PWA

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
@@ -9,6 +10,43 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
+        }),
+        VitePWA({
+          includeAssets: [
+            'favicon.ico',
+            'robots.txt',
+            'apple-touch-icon-180x180.png',
+            'maskable-icon-512x512.png',
+            'logo.svg',
+          ],
+          registerType: 'autoUpdate',
+          manifest: {
+            name: 'Chore Manager',
+            theme_color: '#171717',
+            icons: [
+              {
+                src: 'pwa-64x64.png',
+                sizes: '64x64',
+                type: 'image/png',
+              },
+              {
+                src: 'pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+              },
+              {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+              },
+              {
+                src: 'maskable-icon-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable',
+              },
+            ],
+          },
         }),
     ],
 });
